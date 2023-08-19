24c3470990762ed1366d82671f94e7f4305faabb
DE-corp <crazyHOPHOP@rambler.ru>
2023-08-19 23:33:39 +0300

    Добавил класс User, контроллер UserController и логгер с записью логов в файл

---
+++ AuthenticationService/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace AuthenticationService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger logger;
+        public UserController(ILogger logger)
+        {
+            this.logger = logger;
+
+            logger.WriteEvent("Сообщение о событии в программе");
+            logger.WriteError("Сообщение об ошибки в программе");
+        }
+
+        [HttpGet]
+        public User GetUser()
+        {
+            return new User()
+            {
+
+                Id = Guid.NewGuid(),
+                FirstName = "Dmitry",
+                LastName = "Evdeokimov",
+                Email = "test@test.com",
+                Login = "test",
+                Password = "1234"
+            };
+        }
+    }
+}
---
+++ AuthenticationService/Logger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace AuthenticationService
+{
+    public class Logger : ILogger
+    {
+        private readonly string logsPath = "logs";
+
+        public Logger()
+        {
+            if (!Directory.Exists(logsPath))
+            {
+                Directory.CreateDirectory(logsPath);
+            }
+        }
+
+        public void WriteEvent(string eventMessage)
+        {
+            File.AppendAllText(Path.Combine(logsPath, "events.txt"), eventMessage + Environment.NewLine);
+            Console.WriteLine(eventMessage);
+        }
+
+        public void WriteError(string errorMessage)
+        {
+            File.AppendAllText(Path.Combine(logsPath, "errors.txt"), errorMessage + Environment.NewLine);
+            Console.WriteLine(errorMessage);
+        }
+    }
+}
