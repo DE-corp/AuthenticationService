bc8f9904d34ab0e968998a184aec9fbc87d9a9b9
DE-corp <crazyHOPHOP@rambler.ru>
2023-08-20 18:05:51 +0300

    Создал класс Role, сделал его свойством User

--- AuthenticationService/User.cs
+++ AuthenticationService/User.cs
@@ -10,5 +10,6 @@ namespace AuthenticationService
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public Role Role { get; set; }
     }
 }
