52aa62a2cded2e27a1781ca01722b653dbb12e66
DE-corp <crazyHOPHOP@rambler.ru>
2023-08-20 14:34:53 +0300

    Переделал класс Logger по учебному материалу

--- AuthenticationService/Logger.cs
+++ AuthenticationService/Logger.cs
@@ -1,30 +1,55 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 
 namespace AuthenticationService
 {
     public class Logger : ILogger
     {
-        private readonly string logsPath = "logs";
+        private ReaderWriterLockSlim lock_ = new ReaderWriterLockSlim();
+
+        private string logDirectory { get; set; }
 
         public Logger()
         {
-            if (!Directory.Exists(logsPath))
-            {
-                Directory.CreateDirectory(logsPath);
-            }
+            logDirectory = AppDomain.CurrentDomain.BaseDirectory + @"/_logs/" + DateTime.Now.ToString("dd-MM-yy HH-mm-ss") + @"/";
+
+            if (!Directory.Exists(logDirectory))
+                Directory.CreateDirectory(logDirectory);
         }
 
         public void WriteEvent(string eventMessage)
         {
-            File.AppendAllText(Path.Combine(logsPath, "events.txt"), eventMessage + Environment.NewLine);
-            Console.WriteLine(eventMessage);
+            lock_.EnterWriteLock();
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(logDirectory + "events.txt", append: true))
+                {
+                    writer.WriteLine(eventMessage);
+                }
+            }
+            finally
+            {
+                lock_.ExitWriteLock();
+            }
+
         }
 
         public void WriteError(string errorMessage)
         {
-            File.AppendAllText(Path.Combine(logsPath, "errors.txt"), errorMessage + Environment.NewLine);
-            Console.WriteLine(errorMessage);
+            lock_.EnterWriteLock();
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(logDirectory + "errors.txt", append: true))
+                {
+                    writer.WriteLine(errorMessage);
+                }
+            }
+            finally
+            {
+                lock_.ExitWriteLock();
+            }
+
         }
     }
 }
