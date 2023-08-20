214a753ecaecded2fa0172208542c15a1cb96c06
DE-corp <crazyHOPHOP@rambler.ru>
2023-08-20 17:31:49 +0300

    Установил Swagger

--- AuthenticationService/Startup.cs
+++ AuthenticationService/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace AuthenticationService
 {
@@ -34,6 +35,11 @@ namespace AuthenticationService
             services.AddSingleton<IUserRepository, UserRepository>();
 
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AuthenticationService", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +48,8 @@ namespace AuthenticationService
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AuthenticationService v1"));
             }
 
             app.UseRouting();
